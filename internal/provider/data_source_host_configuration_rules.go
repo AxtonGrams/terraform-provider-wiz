472865aee3abf0c21c11618e84faa9ecf347bb0c
Axton Grams <axton.grams@gmail.com>
2022-10-14 11:52:48 -0400

    fix for go import

--- internal/provider/data_source_host_configuration_rules.go
+++ internal/provider/data_source_host_configuration_rules.go
@@ -11,7 +11,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	//"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 
 	"wiz.io/hashicorp/terraform-provider-wiz/internal"
 	"wiz.io/hashicorp/terraform-provider-wiz/internal/client"
