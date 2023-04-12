337d05a133ea25d216927c694fe1f8a03c808674
Axton Grams <axton.grams@gmail.com>
2023-04-12 16:58:13 -0400

    acceptance test for automation_rule_aws_sns

--- internal/provider/resource_integration_aws_sns_acc_test.go
+++ internal/provider/resource_integration_aws_sns_acc_test.go
@@ -17,7 +17,7 @@ func TestAccResourceWizIntegrationAwsSNS_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"wiz_integration_aws_sns.foo",
 						"name",
-						"test-acc-707c9825-bf45-4d3c-b6a3-97f7e62f541e",
+						"test-acc-WizIntegrationAwsSNS_basic",
 					),
 					resource.TestCheckResourceAttr(
 						"wiz_integration_aws_sns.foo",
@@ -47,7 +47,7 @@ func TestAccResourceWizIntegrationAwsSNS_basic(t *testing.T) {
 
 const testAccResourceWizIntegrationAwsSNS_basic = `
 resource "wiz_integration_aws_sns" "foo" {
-  name                      = "test-acc-707c9825-bf45-4d3c-b6a3-97f7e62f541e"
+  name                      = "test-acc-WizIntegrationAwsSNS_basic"
   aws_sns_topic_arn         = "arn:aws:sns:us-east-1:123456789012:Wiz-Remediation-Issues-Topic"
   aws_sns_access_method     = "ASSUME_SPECIFIED_ROLE"
   aws_sns_customer_role_arn = "arn:aws:iam::123456789012:role/WizAccess-Role"
