fb149e964239031353a9f7c4025ff00bd5bf869d
Nöbel, Jeremia <jeremia.noebel@nerdware.dev>
2024-08-20 15:57:18 +0200

    update docs

--- examples/resources/wiz_saml_group_mapping/resource.tf
+++ examples/resources/wiz_saml_group_mapping/resource.tf
@@ -1,27 +1,65 @@
 # Configure SAML Group Role Mapping on a global scope
 resource "wiz_saml_group_mapping" "test_global_scope" {
-  saml_idp_id       = "test-saml-identity-provider"
-  provider_group_id = "global-reader-group-id"
-  role              = "PROJECT_READER"
+  saml_idp_id = "test-saml-identity-provider"
+  group_mappings = [
+    {
+      provider_group_id = "global-reader-group-id"
+      role              = "PROJECT_READER"
+    }
+  ]
 }
 
 # Configure SAML Group Role Mapping for a single project
 resource "wiz_saml_group_mapping" "test_single_project" {
-  saml_idp_id       = "test-saml-identity-provider"
-  provider_group_id = "admin-group-id"
-  role              = "PROJECT_ADMIN"
-  projects = [
-    "ee25cc95-82b0-4543-8934-5bc655b86786"
+  saml_idp_id = "test-saml-identity-provider"
+  group_mappings = [
+    {
+      provider_group_id = "admin-group-id"
+      role              = "PROJECT_ADMIN"
+      projects = [
+        "ee25cc95-82b0-4543-8934-5bc655b86786"
+      ]
+    }
   ]
 }
 
 # Configure SAML Group Role Mapping for multiple projects
 resource "wiz_saml_group_mapping" "test_multi_project" {
-  saml_idp_id       = "test-saml-identity-provider"
-  provider_group_id = "member-group-id"
-  role              = "PROJECT_MEMBER"
-  projects = [
-    "ee25cc95-82b0-4543-8934-5bc655b86786",
-    "e7f6542c-81f6-43cf-af48-bdd77f09650d"
+  saml_idp_id = "test-saml-identity-provider"
+  group_mappings = [
+    {
+      provider_group_id = "member-group-id"
+      role              = "PROJECT_MEMBER"
+      projects = [
+        "ee25cc95-82b0-4543-8934-5bc655b86786",
+        "e7f6542c-81f6-43cf-af48-bdd77f09650d"
+      ]
+    }
+  ]
+}
+
+# Configure multiple SAML Group Role Mappings
+resource "wiz_saml_group_mapping" "test_multi_mappings" {
+  saml_idp_id = "test-saml-identity-provider"
+  group_mappings = [
+    {
+      provider_group_id = "global-reader-group-id"
+      role              = "PROJECT_READER"
+    },
+    {
+      provider_group_id = "admin-group-id"
+      role              = "PROJECT_ADMIN"
+      projects = [
+        "ee25cc95-82b0-4543-8934-5bc655b86786"
+      ]
+    },
+    {
+      provider_group_id = "member-group-id"
+      role              = "PROJECT_MEMBER"
+      projects = [
+        "ee25cc95-82b0-4543-8934-5bc655b86786",
+        "e7f6542c-81f6-43cf-af48-bdd77f09650d"
+      ]
+    }
   ]
 }
